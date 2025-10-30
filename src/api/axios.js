587802b94c59629804ee7d505be9430a4e643f18
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2025-10-30 22:22:12 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://joineazy-backend.onrender.com/api",
 });
 
 api.interceptors.request.use(
@@ -14,3 +15,4 @@ api.interceptors.request.use(
 );
 
 export default api;
+
